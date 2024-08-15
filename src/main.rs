1a399209d52b1536e22459ee339743683f4b2cd7
green1490 <zambokrisztian1490@gmail.com>
2024-08-15 22:09:14 +0200

    file reading

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,13 @@
 mod model;
+use model::company::Company;
 use csv::Error;
 use csv::Reader;
 
-fn main() {
-    println!("Hello, world!");
+fn main() -> Result<(),Error> {
+    let mut rdr = csv::Reader::from_path("src/data/pest.csv")?;
+    for record in rdr.records() {
+        let result = record?;
+        println!("{}", result.get(1).unwrap());
+    }
+    Ok(())
 }
--- src/model/company.rs
+++ src/model/company.rs
@@ -1,3 +1,7 @@
+use serde::Deserialize;
+
+#[derive(Deserialize, Debug)]
+#[serde(deny_unknown_fields)]
 pub struct Company {
     name: String,
     urls: Vec<String>
